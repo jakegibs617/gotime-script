c9bacc78060054f2552ee5fb7fc7716177d16260
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-05-10 00:10:31 -0400

    first commit

---
+++ hrglass.sh
@@ -0,0 +1,14 @@
+
+# the development environment
+subl work/clients/hrglass/swift-project/hrglass-ios/hrglass
+open work/clients/hrglass/swift-project/hrglass-ios/hrglass.xcworkspace/
+open work/clients/hrglass/
+
+# the web
+open -a "google chrome" https://trello.com/hrglass/members
+open -a "google chrome" --new-tab https://mail.google.com/mail/u/0/#inbox
+open -a "google chrome" --new-tab http://hr.glass/
+
+
+# conversation
+open -a "skype"
